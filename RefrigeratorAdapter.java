fdfb769e410403d5f7ad0256e3cef88a4387effe
Ythan Clarke Orogo <178121170+YthanOrogo@users.noreply.github.com>
2026-09-14 02:52:49 +0000

    Commit

---
+++ RefrigeratorAdapter.java
@@ -0,0 +1,14 @@
+class RefrigeratorAdapter implements PowerOutlet {
+    private Refrigerator refrigerator;
+
+
+    public RefrigeratorAdapter(Refrigerator refrigerator) {
+        this.refrigerator = refrigerator;
+    }
+
+
+    @Override
+    public void plugIn() {
+        refrigerator.startCooling();
+    }
+}
---
+++ SmartphoneAdapter.java
@@ -0,0 +1,14 @@
+class SmartphoneAdapter implements PowerOutlet {
+    private SmartphoneCharger smartphoneCharger;
+
+
+    public SmartphoneAdapter(SmartphoneCharger smartphoneCharger) {
+        this.smartphoneCharger = smartphoneCharger;
+    }
+
+
+    @Override
+    public void plugIn() {
+        smartphoneCharger.chargePhone();
+    }
+}
